59864ba474fff20da33974f0b6e317d6d6850d68
Faith <thao.faith2@gmail.com>
2021-08-11 10:49:42 -0600

    commit tree 37b2348e9af32d60ba7dd4e933ca385555eea5ec

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+function Keypad() {
+    function handleChange (e) {
+        e.preventDefault();
+        console.log('Entering password...')
+    }
+
+    return (
+        <>
+        <input onChange={handleChange} 
+        type="password" 
+        name="password" 
+        placeholder="Enter password..." />
+        </>
+    )
+}
+
+export default Keypad;
